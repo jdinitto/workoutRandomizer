ec2088df9ba012d14508a373a0098c266ca48c30
Jay DiNitto <jay@jaydinitto.com>
2017-09-19 21:19:23 -0400

    Create workoutData.js

---
+++ data/workoutData.js
@@ -0,0 +1,134 @@
+function workoutRandomizerDataReturner () {
+    return {
+        "name": "Jay",
+        "hideNextSets": true,
+        "workouts": {
+            "Chest and Back": [
+                "Standard Push Up",
+                "Wide Front Pull Up",
+                "Military Push Up",
+                "Reverse Grip Pull Up",
+                "Wide Fly Push Up",
+                "Close Grip Pull Up",
+                "Decline Push Up",
+                "Heavy Pants",
+                "Diamond Push Up",
+                "Lawnmower",
+                "Staggered Push Up",
+                "Elbow Out Lawnmower", {
+                    "final": [
+                        "Max Rep Pull Up (any)",
+                        "Max Rep Push Up (any)"
+                    ]
+                }
+            ],
+
+            "Shoulders and Arms": [
+                "Shoulder Press",
+                "In and Out Curl",
+                "Two Arm Tricep Kickback",
+                "Deep Swimmers Press",
+                "Full Supination Curl",
+                "Chair Dip",
+                "Upright Rows",
+                "Static Arm Curl",
+                "Flip Grip Kickback",
+                "Seat 2-angle Shoulder Fly",
+                "Crouching Cohen Curl",
+                "Lying Down Tricep", {
+                    "final": [
+                        "Max Rep Curl",
+                        "Max Rep Chair Dip"
+                    ]
+                }
+            ],
+
+            "Legs and Back": [
+                "Balance Lunge",
+                "Calf Raise Squat",
+                "Reverse Grip Chin Up",
+                "Super Skater",
+                "Wide Front Pull Up",
+                "Step Back Lunge",
+                "Close Grip Pull Up",
+                "Single Leg Squat",
+                "Deadlift Squat",
+                "Switch Grip Pull Up",
+                "Chair Salutation",
+                "Calf Raises",
+                "Leaning/Towel Pull Up", {
+                    "final": [
+                        "Max Rep Pull Up (any)",
+                        "Max Rep Balance Lunge"
+                    ]
+                }
+            ],
+
+            "Chest, Shoulders, and Tris": [
+                "Slow Mo 3 Position Push Up",
+                "In and Out Shoulder Fly",
+                "Chair Dip",
+                "Plange Push Up",
+                "Pike Press",
+                "Two Arm Tricep Kickback",
+                "Floor Fly",
+                "Scarecrow",
+                "Overhead Tricep",
+                "Two-twitch Speed Push Up",
+                "Y Press",
+                "Lying Down Tricep",
+                "Side to Side Push Up",
+                "Pour Fly",
+                "Weighted Circle",
+                "Side-leaning Tricep",
+                "Fly-Row-Press",
+                "Staggered Push Up", {
+                    "final": [
+                        "Max Rep Chair Dip",
+                        "Max Rep Push Up (any)"
+                    ]
+                }
+            ],
+
+            "Back and Biceps": [
+                "Wide Front Pull Up",
+                "Lawnmower",
+                "Twenty Ones",
+                "One Arm Cross Body Curl",
+                "Switch Grip Pull Up",
+                "Elbow Out Lawnmower",
+                "Standing Bicep Curl",
+                "One Arm Concentration Curl",
+                "Corn Cob Pull Up",
+                "Reverse Grip Bent Over Row",
+                "Open Arm Curl",
+                "Static Arm Curl",
+                "Towel/Leaning Pull Up",
+                "Congdon Locomotive",
+                "Crouching Cohen Curl",
+                "Corkscrew",
+                "Chin Up",
+                "Curl Up, Hammer Down",
+                "In and Out Hammer Curl", {
+                    "final": [
+                        "Max Rep Curl",
+                        "Max Rep Pull Up (any)"
+                    ]
+                }
+            ],
+
+            "Core": [
+                "In and Out",
+                "Bicycles (forward and back)",
+                "Crunchy Frog",
+                "Wide Leg Sit Up",
+                "Fifer Scissor",
+                "Hip Rock Raise",
+                "Pulse Up",
+                "V-Up Roll Up",
+                "Oblique V-up (both sides)",
+                "Mason Twist"
+            ]
+        }
+    }
+};
