7804d19dc1e88fc9b86de34cf90183da739a6d23
Jay DiNitto <jay@jaydinitto.com>
2017-09-27 15:03:20 -0400

    Update workoutRandomizer.js

--- js/workoutRandomizer.js
+++ js/workoutRandomizer.js
@@ -6,7 +6,8 @@ workoutRandomizer.workoutButtonMaker = function workoutButtonMakerFunc (workout)
 	var workoutButt = document.createElement('button'),
 		workoutText = document.createTextNode(workout);
 	workoutButt.value = workout;
-	workoutButt.addEventListener("click", function() { 
+	workoutButt.className = 'workoutButt';
+	workoutButt.addEventListener('click', function() { 
 		workoutRandomizer.workoutSelected(workoutButt.value); 
 	});
 	workoutButt.appendChild(workoutText);
@@ -15,48 +16,100 @@ workoutRandomizer.workoutButtonMaker = function workoutButtonMakerFunc (workout)
 
 workoutRandomizer.randomStrings = function randomStringsFunc () {
 	var str = ['Cool','Nice','Sweet','Boss','Boss hog','Killer','Slammin\'','Rad','Gnarly','Schnoggleractor'];
-	return str[0];
+	str = str[workoutRandomizer.randomNumbersArray(1, str.length)];
+	return str;
 };
 
 workoutRandomizer.workoutSelected = function workoutSelectedFunc (workout) {
 	var generatedButts = document.getElementById('workouts').getElementsByTagName('button'),
-		chosenText = '<p>' + workoutRandomizer.randomStrings() + '! You\'re doing ' + workout + '.</p><p>Tap each exercise when you\'re done.</p>',
+		chosenText = '<h2>' + workoutRandomizer.randomStrings() + '! You\'re doing:</h2>' 
+				   + '<h1>' + workout + '</h1>' 
+				   + '<h2>Tap/click each exercise when you\'re done.</h2>',
 		d = workoutRandomizer.data,
 		workouts = d.workouts,
-		i;
+		i, x, z, type, last;
 
 	for (i = 0; i < generatedButts.length; i++) {
-		generatedButts[i].className = "unselectedWorkout";
+		generatedButts[i].className = 'workoutButt unselected';
 		if (generatedButts[i].value === workout) {
-			generatedButts[i].className = "selectedWorkout";
+			generatedButts[i].className = 'workoutButt selected';
 			generatedButts[i].innerHTML = chosenText;
 		} 
 	};
 
-	for (var type in workouts) {
+	for (type in workouts) {
 		if (workouts.hasOwnProperty(type) && type === workout) {
-			var randoNumbers = workoutRandomizer.randomNumbersArray(workouts[type].length, workouts[type].length);
+
+			var workL = workouts[type].length,
+				workoutsEl = document.getElementById('workouts'),
+				randoNumbers, finalSetsMoves;
+
+			finalSets = typeof workouts[type][workL - 1] === 'object';
+
+			finalSets ?
+				randoNumbers = workoutRandomizer.randomNumbersArray(workL - 1, workL - 1):
+				randoNumbers = workoutRandomizer.randomNumbersArray(workL, workL);
+
 			for (x = 0; x < randoNumbers.length; x++) {
 				var set = workouts[type][randoNumbers[x]];
-				if (typeof set !== 'object') {
-					document.getElementById('workouts').appendChild(
-						workoutRandomizer.buildSetButts(set)
-					);
-				};
+				workoutsEl.appendChild(workoutRandomizer.buildSetButts(set));
 			};
+
+			if (finalSets) {
+				finalSetsMoves = workouts[type][workL - 1];
+				for (finalMoves in finalSetsMoves) {
+					for (var z = 0; z < finalSetsMoves[finalMoves].length; z++) {
+						workoutsEl.appendChild(
+							workoutRandomizer.buildSetButts(finalSetsMoves[finalMoves][z])
+						);
+					}	
+				}
+			};
+			document.getElementsByClassName('next')[0].className = 'setButt current';
 		};
+	};
+};
+
+workoutRandomizer.completeSet = function completeSetFunc () {
+	var generatedSets = document.getElementsByClassName('next');
+	for (i = 0; i < generatedSets.length; i++) {
+		generatedSets[0].className = 'setButt current';
 	};
 };
 
 workoutRandomizer.buildSetButts = function buildSetButtsFunc (set) {
 	var setButt = document.createElement('button'),
 		setText = document.createTextNode(set);
-	
-	setButt.addEventListener("click", function() { 
-		//workoutRandomizer.workoutSelected(workoutButt.value); 
+
+	setButt.addEventListener('click', function() { 
+		if (this.className === 'setButt done') {return false};
+
+		var nextButt = document.getElementsByClassName('next')[0],
+			nextTextEl = document.createElement('p'),
+			nextText = workoutRandomizer.randomStrings() + '! Here\'s your next exercise:',
+			nextTextNode = document.createTextNode(nextText),
+			previousTexts = document.getElementsByClassName('msg'),
+			finalMsg = 'You\'re all done! Slam a protein shake and take a shower.',
+			finalMsgNode = document.createTextNode(finalMsg);
+
+		for (i = 0; i < previousTexts.length; i++) {
+			previousTexts[i].className = 'msg done';
+		};
+
+		this.className = 'setButt done';
+		nextTextEl.appendChild(nextTextNode);
+		nextTextEl.className = 'msg';
+
+		if (nextButt === undefined) {
+			document.getElementById('finalMsg').appendChild(finalMsgNode);
+			return;
+		}
+		
+		nextButt.className = 'setButt current';
+		nextButt.parentNode.insertBefore(nextTextEl, nextButt);
 	});
 
-	//number === 0 ? setButt.className = 'setButt next' : setButt.className = 'setButt';
+	setButt.className = 'setButt next';
 	setButt.appendChild(setText);
 	return setButt;
 };
@@ -87,7 +140,7 @@ workoutRandomizer.init = function workoutRandomizerInit () {
 		workoutsEl = document.getElementById('workouts'),
 		workoutButtonEl, i;
 
-	var nameEl = document.getElementById("intro").getElementsByTagName("span");
+	var nameEl = document.getElementById('intro').getElementsByTagName('span');
 	
 	for (i = 0; i < nameEl.length; i++) {
 		nameEl[0].appendChild(userNameNode);
@@ -101,4 +154,4 @@ workoutRandomizer.init = function workoutRandomizerInit () {
 
 window.onload = function init () {
 	workoutRandomizer.init();
-}
+};
